fd73578f300597410cd049290df985f3231161ca
zoyaaskalda <62638933+zoyaaskalda@users.noreply.github.com>
2022-07-19 20:22:36 +0800

    First Commit

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Praktikum_Android\\praktikum_8_android" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src"
+  "PROJECT_DIR=C:\\Praktikum_Android\\praktikum_8_android"
+  "FLUTTER_ROOT=C:\\src"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Praktikum_Android\\praktikum_8_android\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Praktikum_Android\\praktikum_8_android"
+  "FLUTTER_TARGET=C:\\Praktikum_Android\\praktikum_8_android\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Praktikum_Android\\praktikum_8_android\\.dart_tool\\package_config.json"
+)
